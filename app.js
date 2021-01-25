5550d450a6b5b839ec8ae2f5c5bbd418fab28a3e
tiqlmmo9 <tiqlmmo9@gmail.com>
2021-01-25 19:47:50 +0700

    update close to del

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ function toggle_visibility() {
     document.getElementById("search").style.border="none";
     document.getElementById("search__icon").style.color="#FC6514";
     document.getElementById("line").style.display="block";
+    document.getElementById("btn-close").style.display="flex";
     // document.getElementById("search").style.outline="none";
     document.getElementById("search").style.borderRadius="2px";
     document.getElementById("search").style.boxShadow="0 0px 4px 0 rgba(0, 0, 0, 0.2), 0 1px 2px 0 rgba(0, 0, 0, 0.19)";
@@ -23,7 +24,12 @@ function toggle_visibility() {
     // document.getElementById("search").style.border="none";
   }
 
-
+function closetodel(){
+    document.getElementById("input").value = '';
+    toggle_visibility();
+    // document.getElementById("placeholder").style.transform="translateY(+.75rem)";
+    // document.getElementById("placeholder").style.fontSize="1rem";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     var modal = document.getElementById("input-show");
@@ -38,14 +44,16 @@ window.onclick = function(event) {
         // document.getElementById("placeholder").style.position="absolute";
         
         // document.getElementById("search").style.outlineStyle="solid";
-        document.getElementById("search").style.borderRadius="5px";        
+        document.getElementById("search").style.borderRadius="5px";      
+        document.getElementById("search__icon").style.color="#B2BEC3";  
         document.getElementById("search").style.boxShadow="none";
         document.getElementById("search").style.border="solid 1px #B2BEC3";
         document.getElementById("line").style.display="none";
+        // document.getElementById("btn-close").style.display="none";
         if(document.getElementById("input").value != ''){
             // document.getElementById("placeholder").style.color="#FC6514";
             document.getElementById("placeholder").style.fontSize="0.8rem";
-            
+            document.getElementById("btn-close").style.display="flex";
 
             // document.getElementById("placeholder").style.transform="translateY(+.05rem)";
         }
@@ -53,7 +61,6 @@ window.onclick = function(event) {
         document.getElementById("placeholder").style.transform="translateY(+.75rem)";
         document.getElementById("placeholder").style.fontSize="1rem";
         }
-        
     }
     
     
